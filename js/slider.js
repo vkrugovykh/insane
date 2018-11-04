e06e15764b7ce7f6a8bc63aef754800f282cb11e
kva <vasickru@mail.ru>
2018-11-04 18:33:52 +0500

    v 1.5

--- js/slider.js
+++ js/slider.js
@@ -3,7 +3,10 @@ window.addEventListener('DOMContentLoaded', function() {
 
     let page = document.querySelector('.page'),
         pageChildren = page.children,
-        currentScreen = 1;
+        showupContentSlider = document.querySelector('.showup__content-slider'),
+        showupContentSliderItems = showupContentSlider.getElementsByTagName('a'),
+        currentScreen = 1,
+        currentSlide = 1;
 
         page.addEventListener('click', (event) => {
         event.preventDefault();
@@ -17,6 +20,16 @@ window.addEventListener('DOMContentLoaded', function() {
                 return;
             } else if (target.tagName == 'A' && target.classList == '') {
                 firstSlide();
+                return;
+            } else if (target.classList.contains('slick-next')) {
+                currentSlide++;
+                currentSlide = horizontalSlide(showupContentSliderItems, currentSlide - 1);
+                console.log(currentSlide);
+                return;
+            } else if (target.classList.contains('slick-prev')) {
+                currentSlide--;
+                currentSlide = horizontalSlide(showupContentSliderItems, currentSlide - 1);
+                console.log(currentSlide);
                 return;
             }
             target = target.parentNode;
@@ -43,12 +56,56 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 
     function plusSlides(n) {
-        currentScreen = showScreen(currentScreen +=n, pageChildren, currentScreen);
+        currentScreen = showScreen(currentScreen += n, pageChildren, currentScreen);
     }
 
     function firstSlide() {
         currentScreen = 1;
         showScreen(1, pageChildren, currentScreen);
     }
+
+    showupContentSlider.innerHTML += `
+        <button type="button" class="slick-prev">
+            <div class="play__content">
+                <svg width="9" height="11" viewBox="0 0 9 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path opacity="1" d="M0 5.5L9 11V0L0 5.5Z" fill="#EAEAEA"/>
+                </svg>
+            </div>
+        </button>
+        <button type="button" class="slick-next">
+            <div class="play__content">
+                <svg width="9" height="11" viewBox="0 0 9 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path opacity="1" d="M9 5.5L0 11V0L9 5.5Z" fill="#EAEAEA"/>
+                </svg>
+            </div>            
+        </button>`;
+
+
+    horizontalSlide(showupContentSliderItems, currentSlide - 1);
+
+    function horizontalSlide(items, n) {
+
+        if (n >= items.length) {
+            n = 0;
+        };
+        
+        if (n < 0) {
+            n = items.length - 1;
+        };
+
+        for (let i = 0; i < items.length; i++) {
+            if (i < n) {
+                items[i].style.order = items.length - n + i;
+                items[i].classList.remove('card-active');
+            } else if (i == n) {
+                items[i].style.order = i - n;
+                items[i].classList.add('card-active');
+            } else if (i > n) {
+                items[i].style.order = i - n;
+                items[i].classList.remove('card-active');
+            }
+        };
+        return ++n;
+    };
 
 });
